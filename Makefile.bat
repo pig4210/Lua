f97f1adf010b8ad0a7abe07d9a30d834ee7dc27a
pig4210 <7253493@qq.com>
2018-10-11 17:52:25 +0800

    升级5.3.5。改为GNU make编译

--- Makefile.bat
+++ Makefile.bat
@@ -5,8 +5,9 @@
     pushd "%~dp0"
     
 ::baseconfig
-    set VCPATH=C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build
+    set VCPath=C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build
     set MyPath=%CD%
+    set MAKE=%MyPath%/../gnu/make/make.exe
 
     for /d %%P in (.) do set ProjectName=%%~nP
     if "%ProjectName%"=="" (
@@ -18,56 +19,37 @@
     for %%I in (a b c d e f g h i j k l m n o p q r s t u v w x y z) do set ProjectName=!ProjectName:%%I=%%I!
     setlocal disabledelayedexpansion
 
-    for /d %%P in ("%MyPath%\\%ProjectName%*") do set VPATH=%%~fP
-    if "%VPATH%"=="" (
+    for /d %%P in ("%MyPath%\\%ProjectName%*") do set SrcPath=%%~fP
+    if "%SrcPath%"=="" (
         echo !!!!!!!! Src no found !!!!!!!!
         goto end
     )
-    set VPATH=%VPATH%\\src
-    echo ==== ==== ==== ==== Got source folder [ %VPATH% ]
+    echo ==== ==== ==== ==== Got source folder [ %SrcPath% ]
     echo.
 
-::biuldconfig
-    set CC=cl
-    set AR=lib
-    set LNK=link
+    set SrcPath=%SrcPath%/src
 
-    set CFLAGS=/c /MP /GS- /Qpar /GL /analyze- /W4 /Gy /Zc:wchar_t /Zi /Gm- /Ox /Zc:inline /fp:precise /D "WIN32" /D "NDEBUG" /D "_UNICODE" /D "UNICODE" /fp:except- /errorReport:none /GF /WX /Zc:forScope /GR- /Gd /Oy /Oi /MT /EHa /nologo
-
-    set ARFLAGS=/LTCG /ERRORREPORT:NONE /NOLOGO
-
-    set LFLAGS=/MANIFEST:NO /LTCG /NXCOMPAT /DYNAMICBASE "kernel32.lib" "user32.lib" "gdi32.lib" "winspool.lib" "comdlg32.lib" "advapi32.lib" "shell32.lib" "ole32.lib" "oleaut32.lib" "uuid.lib" "odbc32.lib" "odbccp32.lib" /OPT:REF /INCREMENTAL:NO /OPT:ICF /ERRORREPORT:NONE /NOLOGO
+    cl >nul 2>&1
+    if %errorlevel%==0 (
+        set SUF=
+    ) else (
+        set SUF=^>nul
+    )
 
 ::makeinclude
     echo ==== ==== ==== ==== Prepare include folder ^& files...
-    set IncludePath=%MyPath%\\include
 
-    if exist "%IncludePath%" rd /s /q "%IncludePath%" >nul
-    if exist "%IncludePath%" (
-        echo !!!!!!!! Can't clear include folder !!!!!!!!
-        goto end
-    )
-    md "%IncludePath%" >nul
+    call :make inc || goto end
 
-    pushd "%VPATH%"
-    copy "lauxlib.h"    "%IncludePath%\\" >nul
-    copy "lua.h"        "%IncludePath%\\" >nul
-    copy "lua.hpp"      "%IncludePath%\\" >nul
-    copy "luaconf.h"    "%IncludePath%\\" >nul
-    copy "lualib.h"     "%IncludePath%\\" >nul
-    popd
-
-    echo.
-    
 ::main
     echo.
-    %CC% >nul 2>&1
+    cl >nul 2>&1
     if %errorlevel%==0 (
-        echo ==== ==== ==== ==== Build %Platform% ^& processing ==== ==== ==== ==== 
+        echo ==== ==== ==== ==== Build %Platform% ^& processing ==== ==== ==== ====
         echo.
         call :do || goto end
     ) else (
-        echo ==== ==== ==== ==== Build x64 ^& x86 by silence ==== ==== ==== ==== 
+        echo ==== ==== ==== ==== Build x64 ^& x86 by silence ==== ==== ==== ====
         echo.
         call :do x64 || goto end
         call :do x86 || goto end
@@ -102,7 +84,6 @@
         echo !!!!!!!! Need arg with x64/x86 !!!!!!!!
         goto done
     )
-    set GPATH=%MyPath%\\%PLAT%
 
     echo.
 
@@ -118,124 +99,23 @@
         )
     )
 
-    echo ==== ==== ==== ==== Prepare dest folder(%PLAT%)...
-
-    if exist "%GPATH%" rd /s /q "%GPATH%" >nul
-    if exist "%GPATH%" (
-        echo !!!!!!!! Can't clear dest folder !!!!!!!!
-        goto done
-    )
-    md "%GPATH%" >nul
-
-    echo.
-
-    cd /d "%VPATH%"
-
-::localbuildconfig
-    set CFLAGS=%CFLAGS% /wd4244 /wd4310 /wd4324 /wd4702 /D "LUA_COMPAT_ALL" /D"LUA_COMPAT_5_2" /D"LUA_COMPAT_5_1"
-    if "%PLAT%" == "x86" set CFLAGS=%CFLAGS% /D "_USING_V110_SDK71_"
-
-    set ARFLAGS=%ARFLAGS% /MACHINE:%PLAT%
-
-    set LFLAGS=%LFLAGS% /MACHINE:%PLAT%
-
-    if "%PLAT%" == "x64" (
-        set LFLAGS_CONSOLE= /SUBSYSTEM:CONSOLE
-        set LFLAGS_WINDOWS= /SUBSYSTEM:WINDOWS
-    ) else (
-        set LFLAGS_CONSOLE= /SAFESEH /SUBSYSTEM:CONSOLE",5.01"
-        set LFLAGS_WINDOWS= /SAFESEH /SUBSYSTEM:WINDOWS",5.01"
-    )
-
-::lib
-    set CIN="%VPATH%\\*.c"
-
-    set COUT=/Fo"%GPATH%\\" /Fd"%GPATH%\\%ProjectName%lib.pdb"
-
-    set MyCFlags=/D "_LIB"
-
-    set ARIN="%GPATH%\\*.obj"
-
-    set AROUT=/OUT:"%GPATH%\\%ProjectName%lib.lib"
-
-    set MyARFlags=
+    cd /d "%MyPath%"
 
     echo ==== ==== ==== ==== Building LIB(%PLAT%)...
 
-    call :compile || goto done
-    del "%GPATH%\\lua.obj" "%GPATH%\\luac.obj"
-    call :ar || goto done
-
-    del "%GPATH%\\*.obj" >nul
-
-::dll
-    set CIN="%VPATH%\\*.c"
-
-    set COUT=/Fo"%GPATH%\\" /Fd"%GPATH%\\%ProjectName%dll.pdb"
-
-    set MyCFlags=/D "_USRDLL" /D "_WINDLL" /D "LUA_BUILD_AS_DLL"
-
-    set LIN="%GPATH%\\*.obj"
-
-    set LOUT=/OUT:"%GPATH%\\%ProjectName%dll.dll" /IMPLIB:"%GPATH%\\%ProjectName%dll.lib"
-
-    set MyLFlags=/DLL %LFLAGS_WINDOWS%
-
+    call :make lib || goto done
+    
     echo ==== ==== ==== ==== Building DLL(%PLAT%)...
 
-    call :compile || goto done
-    del "%GPATH%\\lua.obj" "%GPATH%\\luac.obj"
-    call :link || goto done
-
-::exe
-    set CIN="%VPATH%\\%ProjectName%.c"
-
-    set COUT=/Fo"%GPATH%\\" /Fd"%GPATH%\\%ProjectName%.pdb"
-
-    set MyCFlags=
-
-    set LIN="%GPATH%\\%ProjectName%.obj" "%GPATH%\\%ProjectName%lib.lib"
-
-    set LOUT=/OUT:"%GPATH%\\%ProjectName%.exe"
-
-    set MyLFlags=%LFLAGS_CONSOLE%
-
+    call :make dll || goto done
+    
     echo ==== ==== ==== ==== Building EXE(%PLAT%)...
 
-    call :compile || goto done
-    call :link || goto done
-
-::exec
-    set CIN="%VPATH%\\%ProjectName%c.c"
-
-    set COUT=/Fo"%GPATH%\\" /Fd"%GPATH%\\%ProjectName%c.pdb"
-
-    set MyCFlags=
-
-    set LIN="%GPATH%\\%ProjectName%c.obj" "%GPATH%\\%ProjectName%lib.lib"
-
-    set LOUT=/OUT:"%GPATH%\\%ProjectName%c.exe"
-
-    set MyLFlags=%LFLAGS_CONSOLE%
+    call :make exe || goto done
 
     echo ==== ==== ==== ==== Building EXEC(%PLAT%)...
 
-    call :compile || goto done
-    call :link || goto done
-
-::dllexe
-    set LIN="%GPATH%\\%ProjectName%.obj" "%GPATH%\\%ProjectName%dll.lib"
-
-    set LOUT=/OUT:"%GPATH%\\%ProjectName%dll.exe"
-
-    set MyLFlags=%LFLAGS_CONSOLE%
-
-    echo ==== ==== ==== ==== Building DLLEXE(%PLAT%)...
-
-    call :link || goto done
-    
-::clear
-    del "%GPATH%\\*.obj" >nul
+    call :make exec || goto done
 
 ::ok
     endlocal
@@ -247,35 +127,13 @@
     echo.
     exit /B 1
 
-:compile
+:make
     if "%SUF%"=="" (
         echo.
-        echo %CC% %CFLAGS% %MyCFlags% %COUT% %CIN%
+        echo %MAKE% SrcPath=%SrcPath% %1
         echo.
     )
-    %CC% %CFLAGS% %MyCFlags% %COUT% %CIN% %SUF% && exit /B 0
+    %MAKE% SRCPATH=%SrcPath% %1 %SUF% && exit /B 0
     
-    echo !!!!!!!! Compile Error !!!!!!!!
-    exit /B 1
-
-:ar
-    if "%SUF%"=="" (
-        echo.
-        echo %AR% %ARFLAGS% %MyARFlags% %AROUT% %ARIN%
-        echo.
-    )
-    %AR% %ARFLAGS% %MyARFlags% %AROUT% %ARIN% %SUF% && exit /B 0
-
-    echo !!!!!!!! AR Error !!!!!!!!
-    exit /B 1
-
-:link
-    if "%SUF%"=="" (
-        echo.
-        echo %LNK% %LFLAGS% %MyLFlags% %LOUT% %LIN%
-        echo.
-    )
-    %LNK% %LFLAGS% %MyLFlags% %LOUT% %LIN% %SUF% && exit /B 0
-
-    echo !!!!!!!! Link Error !!!!!!!!
+    echo !!!!!!!! Make Error !!!!!!!!
     exit /B 1
--- README.md
+++ README.md
@@ -1,21 +1,20 @@
 ﻿# Lua
 
-这里提供的[Makefile.bat](./Makefile.bat)，使用VS2017命令行编译项目
+Lua 虽然提供了原生的 Makefile，但要在 windows 下编译，需要额外安排 MingW 。
 
-如需使用其它VS，请修改如下配置：
+nmake 不支持 VPATH ，前缀路径无法触发默认编译，无特殊宏 `&@` 等。
 
-    set VCPATH=C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build
+本工程是一个特化编译 For Windows 。
 
-源代码目录自动定位，如需指定其它源代码目录，请修改如下代码的VPATH：
+---- ---- ---- ----
 
-    for /d %%P in ("%MyPath%\\%ProjectName%*") do set VPATH=%%~fP
+## 特化编译
 
-在已有VC命令行环境下运行脚本，只编译当前平台相符的库，并且输出编译信息
+参考 Lua 提供的 windows 下的编译手段，实现特化 Makefile 。
 
-无VC命令行环境时，脚本静默编译x64 & x86
+`Makefile` 使用 GNU make 编译 zlib 。
 
-虽然使用Makefile看起来高大上，但这里不采用，原因如下：
-- nmake不支持VPATH，前缀路径无法触发默认编译，无特殊宏“&@”等
-- gmake、dmake对GPATH支持不好，非当前目录的生成结果无法判定，必须写明，且obj没有默认规则
-- 生成结果需要一次性生成x64、x86。用gmake与dmake明显浪费。用nmake又需要开环境显得麻烦
-- Lua虽然提供了原生的makefile，但要在windows下编译，需要额外安排MingW
+`Makefile.bat` 检测当前环境，决定编译结果：
+
+  - 在 对应的编译环境 下，编译对应平台版本，有编译回显。
+  - 无编译环境时，自行编译 x64 & x86 版本，无编译回显。
